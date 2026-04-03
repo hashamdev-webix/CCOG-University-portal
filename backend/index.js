4f659138f83bf4981f66bc9f6ee7bc4abbc94d6c
hashhaam <hashamulhaq068@gmail.com>
2026-04-04 00:00:36 +0500

    fix: skip dotenv in production

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 import express from "express";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
